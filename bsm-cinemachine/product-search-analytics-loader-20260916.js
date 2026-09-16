bb86435e8f0199a1fcf1a5c65483a8dec51dd595
satriaaao <lordeoid0@gmail.com>
2026-09-16 16:39:46 +0700

    feat: load geo analytics before public runtime

---
+++ bsm-cinemachine/product-search-analytics-loader-20260916.js
@@ -0,0 +1,3 @@
+/* Preserve product search, then install geo analytics transport before public CMS runtime executes. */
+document.write('<script src="https://cdn.jsdelivr.net/gh/satriaaao/linix@46bde66fd10ce06d2ba9f0c1af67689bb43c5ba0/bsm-cinemachine/product-search-20260912.js"><\/script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/satriaaao/linix@1f328509e84d179910debed5987d8869b8e604c6/bsm-cinemachine/analytics-geo-interceptor-20260916.js"><\/script>');
