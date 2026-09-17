14effe269942f7beefc9a00e19c273d358e7cc6b
satriaaao <lordeoid0@gmail.com>
2026-09-18 06:59:34 +0700

    refactor(cms): serve rental desk v7 shell

--- bsm-cinemachine/api/cms.js
+++ bsm-cinemachine/api/cms.js
@@ -1,4 +1,4 @@
-const SOURCE='https://cdn.jsdelivr.net/gh/satriaaao/linix@2437c435e5c8a3fd1072e0260301b8df40a3fa95/bsm-cinemachine/cms-v6-20260918.html';
+const SOURCE='https://cdn.jsdelivr.net/gh/satriaaao/linix@9b7d8945fb564c7a40b94d6d96f85ca0a5113aa6/bsm-cinemachine/cms-v7-20260918.html';
 
 async function handler(req,res){
   if(req.method!=='GET'&&req.method!=='HEAD'){
