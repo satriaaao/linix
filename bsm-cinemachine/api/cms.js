a1251e4f48d2bcdf5655c9f85c7428ae387a8a80
satriaaao <lordeoid0@gmail.com>
2026-09-18 19:36:13 +0700

    feat(cms): serve current GPS dispatch start

--- bsm-cinemachine/api/cms.js
+++ bsm-cinemachine/api/cms.js
@@ -1,4 +1,4 @@
-const SOURCE='https://cdn.jsdelivr.net/gh/satriaaao/linix@27a3655038a9e93481073cab475c18d665c3a158/bsm-cinemachine/cms-v15-20260918.html';
+const SOURCE='https://cdn.jsdelivr.net/gh/satriaaao/linix@1082127704354d9da8680e9162be6e4c593122bd/bsm-cinemachine/cms-v15-20260918.html';
 
 async function handler(req,res){
   if(req.method!=='GET'&&req.method!=='HEAD'){
