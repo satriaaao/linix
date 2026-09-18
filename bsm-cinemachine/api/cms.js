97e266245679ce36a9236c28f3865be8c3c02da0
satriaaao <lordeoid0@gmail.com>
2026-09-18 17:16:54 +0700

    feat(cms): serve realtime GPS editable destination

--- bsm-cinemachine/api/cms.js
+++ bsm-cinemachine/api/cms.js
@@ -1,4 +1,4 @@
-const SOURCE='https://cdn.jsdelivr.net/gh/satriaaao/linix@82005c9374b494c929aa10d0002f1601249d5aaf/bsm-cinemachine/cms-v15-20260918.html';
+const SOURCE='https://cdn.jsdelivr.net/gh/satriaaao/linix@e3eb81efad30d485d605a548b2e31045c41666b9/bsm-cinemachine/cms-v15-20260918.html';
 
 async function handler(req,res){
   if(req.method!=='GET'&&req.method!=='HEAD'){
