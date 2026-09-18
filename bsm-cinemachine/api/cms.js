aa4149d231fbe22597323b445a55199f4985a8e6
satriaaao <lordeoid0@gmail.com>
2026-09-18 10:24:01 +0700

    feat(cms): serve latest delivery tracking UI

--- bsm-cinemachine/api/cms.js
+++ bsm-cinemachine/api/cms.js
@@ -1,4 +1,4 @@
-const SOURCE='https://cdn.jsdelivr.net/gh/satriaaao/linix@44478b4a9f7cecf717201768fbfd05b85a3fe588/bsm-cinemachine/cms-v15-20260918.html';
+const SOURCE='https://cdn.jsdelivr.net/gh/satriaaao/linix@ae1ca548959c5f423be37c4f3360ad51d729f1fd/bsm-cinemachine/cms-v15-20260918.html';
 
 async function handler(req,res){
   if(req.method!=='GET'&&req.method!=='HEAD'){
